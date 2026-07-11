2d98b742182af318ef20965d0189535895794ff7
ANANYA SINGH <ananya35553@gmail.com>
2026-07-11 14:22:48 +0530

    Add files via upload

---
+++ FloydTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class FloydTriangle
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter value of n:");
+        int n=sc.nextInt();
+        int k=1;
+        for (int r = 1; r <= n; r++) {
+            for (int c = 1; c <= r; c++) {
+                System.out.print(k + " ");
+                k++;
+            }
+            System.out.print("\n");
+        }
+    }
+}
